b0af97992f3b22476024754213c56f61e19cd4a5
g4m4 <misept.dieseneuf@gmail.com>
2022-06-09 15:26:23 +0200

    First working example with asmdef etc.

--- Paths.cs
+++ Paths.cs
@@ -5,13 +5,41 @@ namespace StudioManette
 {
     namespace Utils
     {
-        public class Path
+        // Named "Paths" so there is no collision with System.IO.Path
+        public static class Paths
         {
+            // From the given string input, retrieve a string suitable for a file name
+            // For instance this string: Toto"a>b<c
+            // will yield: Toto_a_b_c
             public static string SanitiseFilename(string input)
             {
                 return String.Join("_", input.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries));
             }
 
+            // Attemps to retrieve the path "toPath" relative from "fromPath"
+            //
+            // Typical example
+            // with:
+            // fromPath = Application.dataPath (= "D:/00_MANETTE/Edna_main/Assets")
+            // toPath = "D:/00_MANETTE/Edna_main/Assets/Renderer/Textures/HDRI/HDRI_01.psd"
+            // it yields:
+            // "Assets\Renderer\Textures\HDRI\HDRI_01.psd"
+            //
+            // Beware, it can get tricky!
+            //
+            // GOTCHA: beware of trailing slashes!
+            // With:
+            // fromPath = "D:/00_MANETTE/Edna_main/Assets/" (exactly the same as the above example + a trailing "/")
+            // toPath = "D:/00_MANETTE/Edna_main/Assets/Renderer/Textures/HDRI/HDRI_01.psd"
+            // it yields:
+            // "Renderer\Textures\HDRI\HDRI_01.psd"
+            //
+            // GOTCHA: matching seemingly unrelated paths
+            // With:
+            // fromPath = Application.dataPath (= "D:/00_MANETTE/Edna_main/Assets")
+            // toPath = "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipelineResources/Texture/BlueNoise16/L/LDR_LLL1_25.png"
+            // it yields:
+            // "Library\PackageCache\com.unity.render-pipelines.high-definition@12.1.6\Runtime\RenderPipelineResources\Texture\BlueNoise16\L\LDR_LLL1_25.png"
             public static string GetRelativePath(string fromPath, string toPath)
             {
                 // Yay, yet another shoutout to https://stackoverflow.com/questions/275689/how-to-get-relative-path-from-absolute-path
