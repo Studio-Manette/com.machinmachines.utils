28528a87335f942c8f0d437f7ed7345562477a79
Gabriel Bonnefond <gabriel.bonnefond@gmail.com>
2022-06-27 16:12:27 +0200

    [EDITOASSETS] add function FindAssetsByType<>

---
+++ EditorAssets.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+
+namespace StudioManette
+{
+    namespace Utils
+    {
+        public static class EditorAssets
+        {
+            public static List<T> FindAssetsByType<T>() where T : UnityEngine.Object
+            {
+                List<T> assets = new List<T>();
+                string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
+                for (int i = 0; i < guids.Length; i++)
+                {
+                    string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                    T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                    if (asset != null)
+                    {
+                        assets.Add(asset);
+                    }
+                }
+                return assets;
+            }
+        }
+    }
+}
