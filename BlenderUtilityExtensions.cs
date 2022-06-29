4b9f318b4eb84a4843c44e29a4851622dd94cd58
g4m4 <misept.dieseneuf@gmail.com>
2022-06-29 17:06:18 +0200

    [BLENDERUTILITY] Moved from Bob

---
+++ BlenderUtilityExtensions.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace StudioManette
+{
+    namespace Bob
+    {
+        namespace Common
+        {
+            public static class BlenderUtilityExtensions
+            {
+                public static Vector3 FromUnityToBlender(this Vector3 positionVector)
+                {
+                    return new Vector3(-positionVector.x, -positionVector.z, positionVector.y);
+                }
+
+                public static Vector3 FromBlenderToUnity(this Vector3 positionVector)
+                {
+                    return new Vector3(-positionVector.x, positionVector.z, -positionVector.y);
+                }
+
+                // Apply the input PRS data to the given transform
+                public static void FromBlenderToUnityEulerAngles(
+                    this Transform existingTransform,
+                    Vector3 blenderPosition,
+                    Vector3 blenderRotation,
+                    Vector3 blenderScale)
+                {
+                    existingTransform.localPosition = blenderPosition;
+                    if (true)//master.rotation.eulerAngles != rot)
+                    {
+                        Vector3 convertedRot = -1.0f * blenderRotation.FromBlenderToUnity();
+                        existingTransform.localEulerAngles = new Vector3(existingTransform.localEulerAngles.x,
+                                                                         0.0f,
+                                                                         existingTransform.localEulerAngles.z);
+                        existingTransform.localEulerAngles += convertedRot;
+                    }
+                    existingTransform.localScale = blenderScale;
+                }
+            }
+        }
+    }
+}
