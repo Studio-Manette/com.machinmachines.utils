be27530bc0fe0506321e3421d8f65aef923aed43
g4m4 <misept.dieseneuf@gmail.com>
2022-09-20 11:15:08 +0200

    [COMMON] Removed extraneous namespace

--- Common/BlenderUtilityExtensions.cs
+++ Common/BlenderUtilityExtensions.cs
@@ -2,40 +2,37 @@ using UnityEngine;
 
 namespace StudioManette
 {
-    namespace Bob
+    namespace Common
     {
-        namespace Common
+        public static class BlenderUtilityExtensions
         {
-            public static class BlenderUtilityExtensions
+            public static Vector3 FromUnityToBlender(this Vector3 positionVector)
             {
-                public static Vector3 FromUnityToBlender(this Vector3 positionVector)
-                {
-                    return new Vector3(-positionVector.x, -positionVector.z, positionVector.y);
-                }
+                return new Vector3(-positionVector.x, -positionVector.z, positionVector.y);
+            }
 
-                public static Vector3 FromBlenderToUnity(this Vector3 positionVector)
-                {
-                    return new Vector3(-positionVector.x, positionVector.z, -positionVector.y);
-                }
+            public static Vector3 FromBlenderToUnity(this Vector3 positionVector)
+            {
+                return new Vector3(-positionVector.x, positionVector.z, -positionVector.y);
+            }
 
-                // Apply the input PRS data to the given transform
-                public static void FromBlenderToUnity(
-                    this Transform existingTransform,
-                    Vector3 blenderPosition,
-                    Vector3 blenderRotation,
-                    Vector3 blenderScale)
+            // Apply the input PRS data to the given transform
+            public static void FromBlenderToUnity(
+                this Transform existingTransform,
+                Vector3 blenderPosition,
+                Vector3 blenderRotation,
+                Vector3 blenderScale)
+            {
+                existingTransform.localPosition = blenderPosition.FromBlenderToUnity();
+                if (true)//master.rotation.eulerAngles != rot)
                 {
-                    existingTransform.localPosition = blenderPosition.FromBlenderToUnity();
-                    if (true)//master.rotation.eulerAngles != rot)
-                    {
-                        Vector3 convertedRot = -1.0f * blenderRotation.FromBlenderToUnity();
-                        existingTransform.localEulerAngles = new Vector3(existingTransform.localEulerAngles.x,
-                                                                         0.0f,
-                                                                         existingTransform.localEulerAngles.z);
-                        existingTransform.localEulerAngles += convertedRot;
-                    }
-                    existingTransform.localScale = blenderScale;
+                    Vector3 convertedRot = -1.0f * blenderRotation.FromBlenderToUnity();
+                    existingTransform.localEulerAngles = new Vector3(existingTransform.localEulerAngles.x,
+                                                                        0.0f,
+                                                                        existingTransform.localEulerAngles.z);
+                    existingTransform.localEulerAngles += convertedRot;
                 }
+                existingTransform.localScale = blenderScale;
             }
         }
     }
