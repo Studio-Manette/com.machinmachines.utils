78c8620c54262244347aa10f6b33e274e862b78a
g4m4 <misept.dieseneuf@gmail.com>
2022-11-18 18:15:35 +0100

    [DGML] Added a super simple DGML serialiser

---
+++ Editor/DGML/DGMLSerialiser.cs
@@ -0,0 +1,61 @@
+// Copyright 2022 MachinMachines
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MachinMachines
+{
+    namespace DGML
+    {
+        public class DGMLNode
+        {
+            [XmlAttribute]
+            public string Id;
+            [XmlAttribute]
+            public string Label;
+        }
+
+        [XmlRoot(Namespace = @"http://schemas.microsoft.com/vs/2009/dgml")]
+        public class DirectedGraph
+        {
+            [XmlAttribute]
+            public string Title;
+
+            [XmlArray("Nodes")]
+            [XmlArrayItem("Node")]
+            public DGMLNode[] Nodes;
+
+            public int Links { get; set; }
+
+            public int Categories { get; set; }
+
+            public int Properties { get; set; }
+        }
+
+        public class DGMLSerialiser
+        {
+            public const string Extension = "dgml";
+
+            public static void SerialiseToPath(DirectedGraph graph, string filepath)
+            {
+                using(StreamWriter stream = new StreamWriter(filepath))
+                {
+                    XmlSerializer xmls = new XmlSerializer(typeof(DirectedGraph));
+                    xmls.Serialize(stream, graph);
+                }
+            }
+        }
+    }
+}
