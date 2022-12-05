efe394e668cc5acf8b016703579d9e1c04f6f71a
g4m4 <misept.dieseneuf@gmail.com>
2022-12-05 18:38:18 +0100

    [MISC] Namespace formatting

--- Editor/DGML/DGMLSerialiser.cs
+++ Editor/DGML/DGMLSerialiser.cs
@@ -19,189 +19,186 @@ using System.Xml.Serialization;
 
 using UnityEngine;
 
-namespace MachinMachines
+namespace MachinMachines.DGML
 {
-    namespace DGML
+    // Super basic class for organising data as a hierarchical tree
+    // Only useful as inheriting classes can then be automatically dumped as DGML
+    public abstract class HierarchicalTreeItem<T> where T : class
     {
-        // Super basic class for organising data as a hierarchical tree
-        // Only useful as inheriting classes can then be automatically dumped as DGML
-        public abstract class HierarchicalTreeItem<T> where T : class
+        // To be overridden by inheriting classes
+        public abstract string Name { get; }
+        // (optional) To be overridden by inheriting classes if need be
+        public virtual string Category { get { return ""; } }
+        // (optional) To be overridden by inheriting classes if need be
+        public virtual Dictionary<string, Color> CategoryToColorMapping { get { return null; } }
+
+        public HierarchicalTreeItem<T>[] children = new HierarchicalTreeItem<T>[0];
+        // Same as the above but with the correct derived type
+        public IEnumerable<T> DirectChildren
         {
-            // To be overridden by inheriting classes
-            public abstract string Name { get; }
-            // (optional) To be overridden by inheriting classes if need be
-            public virtual string Category { get { return ""; } }
-            // (optional) To be overridden by inheriting classes if need be
-            public virtual Dictionary<string, Color> CategoryToColorMapping { get { return null; } }
-
-            public HierarchicalTreeItem<T>[] children = new HierarchicalTreeItem<T>[0];
-            // Same as the above but with the correct derived type
-            public IEnumerable<T> DirectChildren
+            get
             {
-                get
-                {
-                    return children.Select(item => item as T);
-                }
-            }
-
-            public DirectedGraph GenerateDirectedGraph()
-            {
-                DirectedGraph result = new DirectedGraph { Title = $"{Name}_graph" };
-                // Using only links (from child to parent)
-                IEnumerable<(HierarchicalTreeItem<T>, HierarchicalTreeItem<T>)> links = ConstructLinks_r(this);
-                // This way we can easily get all items (all distinct children and parents)
-                HashSet<HierarchicalTreeItem<T>> allItems = links.Select(item => item.Item1)
-                                                                 .Union(links.Select(item => item.Item2))
-                                                                 .ToHashSet();
-                // Then hand it over to DGML containers
-                result.Nodes = allItems
-                    .Select(item => new DGMLNode
-                    {
-                        Id = item.Name,
-                        Category = !string.IsNullOrEmpty(item.Category) ? item.Category : null
-                    })
-                    .ToHashSet(new DGMLNodeComparer());
-                result.Links = links
-                    .Select(item => new DGMLLink
-                    {
-                        Source = item.Item1.Name,
-                        Target = item.Item2.Name
-                    })
-                    .ToHashSet();
-                // If there is a lookup table setup, map every category to a color
-                if (CategoryToColorMapping != null)
-                {
-                    result.Categories = result.Nodes.GroupBy(item => item.Category)
-                                                  .Select(item => new DGMLCategory
-                                                  {
-                                                      Id = item.Key,
-                                                      Background = $"#ff{ColorUtility.ToHtmlStringRGB(CategoryToColorMapping[item.Key])}"
-                                                  })
-                                                  .ToHashSet();
-                }
-                return result;
-            }
-
-            public void DumpAsDGMLToPath(string graphPath)
-            {
-                DirectedGraph graph = GenerateDirectedGraph();
-                graphPath = Path.ChangeExtension(graphPath, DGMLSerialiser.Extension);
-                DGMLSerialiser.SerialiseToPath(graph, graphPath);
-            }
-
-            // Generator retrieving all children items, including the root one, as a flattened list
-            public IEnumerable<HierarchicalTreeItem<T>> GetAllItems_r()
-            {
-                yield return this;
-                foreach (HierarchicalTreeItem<T> child in children)
-                {
-                    foreach (HierarchicalTreeItem<T> item in child.GetAllItems_r())
-                    {
-                        yield return item;
-                    }
-                }
-            }
-
-            // Build a list of all "links" (dependency relationship) between all resources recursively
-            private IEnumerable<(HierarchicalTreeItem<T>, HierarchicalTreeItem<T>)> ConstructLinks_r(HierarchicalTreeItem<T> root)
-            {
-                List<(HierarchicalTreeItem<T>, HierarchicalTreeItem<T>)> result = new(root.children.Length);
-                foreach (HierarchicalTreeItem<T> child in root.children)
-                {
-                    result.Add((child, root));
-                    result.AddRange(ConstructLinks_r(child));
-                }
-                return result;
+                return children.Select(item => item as T);
             }
         }
 
-        // A simple helper so we can consider two DGML nodes are equal
-        // as long as they have "equivalent" names (equal whatever the case)
-        public class DGMLNodeComparer : IEqualityComparer<DGMLNode>
+        public DirectedGraph GenerateDirectedGraph()
         {
-            public bool Equals(DGMLNode lhs, DGMLNode rhs)
-            {
-                // Check whether the objects are the same object.
-                if (lhs.Equals(rhs))
+            DirectedGraph result = new DirectedGraph { Title = $"{Name}_graph" };
+            // Using only links (from child to parent)
+            IEnumerable<(HierarchicalTreeItem<T>, HierarchicalTreeItem<T>)> links = ConstructLinks_r(this);
+            // This way we can easily get all items (all distinct children and parents)
+            HashSet<HierarchicalTreeItem<T>> allItems = links.Select(item => item.Item1)
+                                                             .Union(links.Select(item => item.Item2))
+                                                             .ToHashSet();
+            // Then hand it over to DGML containers
+            result.Nodes = allItems
+                .Select(item => new DGMLNode
                 {
-                    return true;
-                }
-                return lhs.Id.ToLower().Equals(rhs.Id.ToLower());
-            }
-
-            public int GetHashCode(DGMLNode obj)
-            {
-                return obj.Id.ToLower().GetHashCode();
-            }
-        }
-
-        public class DGMLNode
-        {
-            [XmlAttribute]
-            public string Id;
-
-            [XmlAttribute]
-            public string Category;
-
-            [XmlAttribute]
-            public string Group = null;
-            [XmlIgnore]
-            public bool GroupSpecified { get { return this.Group != null; } }
-        }
-
-        public class DGMLLink
-        {
-            [XmlAttribute]
-            public string Source;
-            [XmlAttribute]
-            public string Target;
-
-            [XmlAttribute]
-            public string Category = null;
-            [XmlIgnore]
-            public bool CategorySpecified { get { return this.Category != null; } }
-        }
-
-        public class DGMLCategory
-        {
-            [XmlAttribute]
-            public string Id;
-            [XmlAttribute]
-            public string Background;
-        }
-
-        [XmlRoot(Namespace = @"http://schemas.microsoft.com/vs/2009/dgml")]
-        public class DirectedGraph
-        {
-            [XmlAttribute]
-            public string Title;
-
-            [XmlArray("Nodes")]
-            [XmlArrayItem("Node")]
-            public HashSet<DGMLNode> Nodes;
-
-            [XmlArray("Links")]
-            [XmlArrayItem("Link")]
-            public HashSet<DGMLLink> Links;
-
-            [XmlArray("Categories")]
-            [XmlArrayItem("Category")]
-            public HashSet<DGMLCategory> Categories;
-
-            public int Properties { get; set; }
-        }
-
-        public class DGMLSerialiser
-        {
-            public const string Extension = "dgml";
-
-            public static void SerialiseToPath(DirectedGraph graph, string filepath)
-            {
-                using(StreamWriter stream = new StreamWriter(filepath))
+                    Id = item.Name,
+                    Category = !string.IsNullOrEmpty(item.Category) ? item.Category : null
+                })
+                .ToHashSet(new DGMLNodeComparer());
+            result.Links = links
+                .Select(item => new DGMLLink
                 {
-                    XmlSerializer xmls = new XmlSerializer(typeof(DirectedGraph));
-                    xmls.Serialize(stream, graph);
+                    Source = item.Item1.Name,
+                    Target = item.Item2.Name
+                })
+                .ToHashSet();
+            // If there is a lookup table setup, map every category to a color
+            if (CategoryToColorMapping != null)
+            {
+                result.Categories = result.Nodes.GroupBy(item => item.Category)
+                                              .Select(item => new DGMLCategory
+                                              {
+                                                  Id = item.Key,
+                                                  Background = $"#ff{ColorUtility.ToHtmlStringRGB(CategoryToColorMapping[item.Key])}"
+                                              })
+                                              .ToHashSet();
+            }
+            return result;
+        }
+
+        public void DumpAsDGMLToPath(string graphPath)
+        {
+            DirectedGraph graph = GenerateDirectedGraph();
+            graphPath = Path.ChangeExtension(graphPath, DGMLSerialiser.Extension);
+            DGMLSerialiser.SerialiseToPath(graph, graphPath);
+        }
+
+        // Generator retrieving all children items, including the root one, as a flattened list
+        public IEnumerable<HierarchicalTreeItem<T>> GetAllItems_r()
+        {
+            yield return this;
+            foreach (HierarchicalTreeItem<T> child in children)
+            {
+                foreach (HierarchicalTreeItem<T> item in child.GetAllItems_r())
+                {
+                    yield return item;
                 }
+            }
+        }
+
+        // Build a list of all "links" (dependency relationship) between all resources recursively
+        private IEnumerable<(HierarchicalTreeItem<T>, HierarchicalTreeItem<T>)> ConstructLinks_r(HierarchicalTreeItem<T> root)
+        {
+            List<(HierarchicalTreeItem<T>, HierarchicalTreeItem<T>)> result = new(root.children.Length);
+            foreach (HierarchicalTreeItem<T> child in root.children)
+            {
+                result.Add((child, root));
+                result.AddRange(ConstructLinks_r(child));
+            }
+            return result;
+        }
+    }
+
+    // A simple helper so we can consider two DGML nodes are equal
+    // as long as they have "equivalent" names (equal whatever the case)
+    public class DGMLNodeComparer : IEqualityComparer<DGMLNode>
+    {
+        public bool Equals(DGMLNode lhs, DGMLNode rhs)
+        {
+            // Check whether the objects are the same object.
+            if (lhs.Equals(rhs))
+            {
+                return true;
+            }
+            return lhs.Id.ToLower().Equals(rhs.Id.ToLower());
+        }
+
+        public int GetHashCode(DGMLNode obj)
+        {
+            return obj.Id.ToLower().GetHashCode();
+        }
+    }
+
+    public class DGMLNode
+    {
+        [XmlAttribute]
+        public string Id;
+
+        [XmlAttribute]
+        public string Category;
+
+        [XmlAttribute]
+        public string Group = null;
+        [XmlIgnore]
+        public bool GroupSpecified { get { return this.Group != null; } }
+    }
+
+    public class DGMLLink
+    {
+        [XmlAttribute]
+        public string Source;
+        [XmlAttribute]
+        public string Target;
+
+        [XmlAttribute]
+        public string Category = null;
+        [XmlIgnore]
+        public bool CategorySpecified { get { return this.Category != null; } }
+    }
+
+    public class DGMLCategory
+    {
+        [XmlAttribute]
+        public string Id;
+        [XmlAttribute]
+        public string Background;
+    }
+
+    [XmlRoot(Namespace = @"http://schemas.microsoft.com/vs/2009/dgml")]
+    public class DirectedGraph
+    {
+        [XmlAttribute]
+        public string Title;
+
+        [XmlArray("Nodes")]
+        [XmlArrayItem("Node")]
+        public HashSet<DGMLNode> Nodes;
+
+        [XmlArray("Links")]
+        [XmlArrayItem("Link")]
+        public HashSet<DGMLLink> Links;
+
+        [XmlArray("Categories")]
+        [XmlArrayItem("Category")]
+        public HashSet<DGMLCategory> Categories;
+
+        public int Properties { get; set; }
+    }
+
+    public class DGMLSerialiser
+    {
+        public const string Extension = "dgml";
+
+        public static void SerialiseToPath(DirectedGraph graph, string filepath)
+        {
+            using (StreamWriter stream = new StreamWriter(filepath))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(DirectedGraph));
+                xmls.Serialize(stream, graph);
             }
         }
     }
